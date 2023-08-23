242e4e3a066721aa28841e1b6753534dcc07153e
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-08-23 07:08:11 +0300

    updata and add some file

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -1,71 +1,30 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable react-native/no-inline-styles */
-import {View, Platform} from 'react-native';
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Feather} from '@expo/vector-icons'; // Import icons from a suitable library
-const Tab = createBottomTabNavigator();
-const menuIcons = (route, focused) => {
-  let icon;
+import {NavigationContainer} from '@react-navigation/native';
+import TabNavigation from './TabNavigation';
+import Product from '../screen/Product';
 
-  if (route.name === 'home') {
-    icon = <Feather name="home" size={30} color={focused ? 'blue' : 'white'} />;
-  } else if (route.name === 'favourite') {
-    icon = <Feather name="heart" size={30} color={focused ? 'red' : 'white'} />;
-  } else if (route.name === 'cart') {
-    icon = (
-      <Feather
-        name="shopping-cart"
-        size={30}
-        color={focused ? 'green' : 'white'}
-      />
-    );
-  }
-
-  return (
-    <View
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        backgroundColor: focused ? 'blue' : 'transparent',
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-      }}>
-      {icon}
-    </View>
-  );
-};
+const Stack = createNativeStackNavigator();
 
 const StackNavigation = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarIcon: ({focused}) => menuIcons(route, focused),
-        tabBarStyle: {
-          marginBottom: 20,
-          height: 75,
-          alignItems: 'center',
-          borderRadius: 100,
-          marginHorizontal: 20,
-          backgroundColor: 'white',
-        },
-        tabBarItemStyle: {
-          marginTop: Platform.OS === 'ios' ? 30 : 0,
-        },
-      })}>
-      <Tab.Screen name="home" component={HomeScreen} />
-      <Tab.Screen name="favorite" component={HomeScreen} />
-      <Tab.Screen name="cart" component={HomeScreen} />
-    </Tab.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          contentStyle: {backgroundColor: 'white'},
+        }}>
+        <Stack.Screen
+          name="TabNavigation"
+          options={{headerShown: false}}
+          component={TabNavigation}
+        />
+        <Stack.Screen
+          name="Product"
+          options={{headerShown: false}}
+          component={Product}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,30 +1,30 @@
 /* eslint-disable prettier/prettier */
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './../screen/Home';
+import Product from './../screen/Product';
 
 const Stack = createNativeStackNavigator();
 
 const TabNavigation = () => {
   return (
- <NavigationContainer>
-  <Stack.Navigator
-    screenOptions={{
-      contentStyle: {backgroundColor: 'white'},
-    }}>
-    <Stack.Screen
-      name="Home"
-      options={{headerShown: false}}
-      component={HomeTabs}
-    />
-    <Stack.Screen
-      name="Product"
-      options={{headerShown: false}}
-      component={ProductScreen}
-    />
-  </Stack.Navigator>
-</NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          contentStyle: {backgroundColor: 'white'},
+        }}>
+        <Stack.Screen
+          name="Home"
+          options={{headerShown: false}}
+          component={Home}
+        />
+        <Stack.Screen
+          name="Product"
+          options={{headerShown: false}}
+          component={Product}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
