7d7d6146b7a12e18064a6f112c07671d172b3c17
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-08-30 21:48:20 +0300

    feact the data

--- src/screen/admin/AdminAddItems.tsx
+++ src/screen/admin/AdminAddItems.tsx
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Alert, StyleSheet, ScrollView, Text, KeyboardAvoidingView, TouchableOpacity, Image } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  ScrollView,
+  Text,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import SQLite from 'react-native-sqlite-2';
-import { coffeeCategories } from '../../data/data';
-import { Picker } from '@react-native-picker/picker'; // Import Picker from the new package
+import {coffeeCategories} from '../../data/data';
+import {Picker} from '@react-native-picker/picker'; // Import Picker from the new package
 import {launchImageLibrary} from 'react-native-image-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { useDarkMode } from '../../hooks/useDarkMode';
-
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {useDarkMode} from '../../hooks/useDarkMode';
 
 const db = SQLite.openDatabase('users.db');
 
 const AddCoffeeScreen = ({navigation}) => {
-   const isDarkMode = useDarkMode();
+  const isDarkMode = useDarkMode();
   const [selectedCategory, setSelectedCategory] = useState('');
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
@@ -29,18 +42,18 @@ const AddCoffeeScreen = ({navigation}) => {
 
     db.transaction(tx => {
       tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS coffeeData (id TEXT PRIMARY KEY, categoryId TEXT, name TEXT, description TEXT, imageUrl TEXT, price TEXT, ingredients TEXT, servingSize TEXT, caffeineContent TEXT, origin TEXT, roastLevel TEXT)',
-        []
+        'CREATE TABLE IF NOT EXISTS coffeeData (id TEXT PRIMARY KEY, categoryId TEXT, name TEXT, description TEXT, imageUri TEXT, price TEXT, ingredients TEXT, servingSize TEXT, caffeineContent TEXT, origin TEXT, roastLevel TEXT)',
+        [],
       );
 
       tx.executeSql(
-        'INSERT INTO coffeeData (id, categoryId, name, description, imageUrl, price, ingredients, servingSize, caffeineContent, origin, roastLevel) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        'INSERT INTO coffeeData (id, categoryId, name, description, imageUri, price, ingredients, servingSize, caffeineContent, origin, roastLevel) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
         [
           coffeeId,
           selectedCategory,
           name,
           description,
-          imageUrl,
+          imageUri,
           price,
           ingredients,
           servingSize,
@@ -51,19 +64,24 @@ const AddCoffeeScreen = ({navigation}) => {
         (_, results) => {
           if (results.rowsAffected > 0) {
             Alert.alert('Success', 'Coffee item added successfully');
+            navigation.goBack();
+            
+         
           } else {
             Alert.alert('Error', 'Failed to add coffee item');
           }
         },
         (_, error) => {
           Alert.alert('Error', 'An error occurred while adding coffee item');
-        }
+          console.log('====================================');
+          console.log(error);
+          console.log('====================================');
+        },
       );
     });
   };
 
-
-    const handleImageSelection = () => {
+  const handleImageSelection = () => {
     const options = {
       mediaType: 'photo',
       quality: 1,
@@ -82,171 +100,190 @@ const AddCoffeeScreen = ({navigation}) => {
     });
   };
 
+  const styles = StyleSheet.create({
+    container: {
+      padding: wp(5),
+      flex: 1,
+      backgroundColor: isDarkMode ? '#000' : '#fff',
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: hp(3),
+      paddingHorizontal: wp(2),
+    },
+    scrollView: {
+      flex: 1,
+      paddingHorizontal: wp(5),
+    },
+    headerText: {
+      fontSize: wp(5),
+      marginLeft: wp(2),
+      color: '#fff',
+    },
+    column: {
+      flexDirection: 'column',
+      marginBottom: hp(3),
+    },
+    picker: {
+      marginBottom: hp(2),
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: wp(4),
 
-const styles = StyleSheet.create({
-  container: {
-    padding: wp(5),
-    flex: 1,
-    backgroundColor: isDarkMode ? '#000' : '#fff',
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: hp(3),
-    paddingHorizontal: wp(2),
-  },
-  scrollView: {
-    flex: 1,
-    paddingHorizontal: wp(5),
-  },
-  headerText: {
-    fontSize: wp(5),
-    marginLeft: wp(2),
-    color: '#fff',
-  },
-  column: {
-    flexDirection: 'column',
-    marginBottom: hp(3),
-  },
-  picker: {
-    marginBottom: hp(2),
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: wp(4),
-
-    paddingHorizontal: wp(2),
-    paddingVertical: hp(1),
-    fontSize: wp(4),
-       backgroundColor:  '#955629',
-    color: '#fff',
-
-
- 
-  },
-  input: {
-    marginBottom: hp(2),
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: wp(2),
-    paddingHorizontal: wp(2),
-    paddingVertical: hp(1),
-    fontSize: wp(4),
-    color: '#000',
-    backgroundColor: isDarkMode ? '#555' : '#f0f0f0',
-    
-  },
-  uploadImage: {
-    marginBottom: hp(3),
-  },
-  imageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  image: {
-    width: wp(25),
-    height: wp(25),
-    resizeMode: 'cover',
-    borderRadius: wp(20),
-    marginRight: wp(2),
-  },
-  uploadText: {
-    color: isDarkMode ? 'white' : 'black',
-  },
-  button: {
-    backgroundColor: '#955629',
-    padding: hp(2),
-    borderRadius: 8,
-    alignItems: 'center',
-    marginTop: hp(3),
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: wp(4),
-  },
-
-});
+      paddingHorizontal: wp(2),
+      paddingVertical: hp(1),
+      fontSize: wp(4),
+      backgroundColor: '#955629',
+      color: '#fff',
+    },
+    input: {
+      marginBottom: hp(2),
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: wp(2),
+      paddingHorizontal: wp(2),
+      paddingVertical: hp(1),
+      fontSize: wp(4),
+      color: '#000',
+      backgroundColor: isDarkMode ? '#555' : '#f0f0f0',
+    },
+    uploadImage: {
+      marginBottom: hp(3),
+    },
+    imageContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    image: {
+      width: wp(25),
+      height: wp(25),
+      resizeMode: 'cover',
+      borderRadius: wp(20),
+      marginRight: wp(2),
+    },
+    uploadText: {
+      color: isDarkMode ? 'white' : 'black',
+      fontSize: wp(5),
+      marginLeft: 5,
+    },
+    exampleText: {
+      color: isDarkMode ? '#fff' : '#000',
+      fontSize: wp(4),
+      marginBottom: hp(1),
+    },
+  });
   return (
-      <KeyboardAvoidingView
-    style={styles.container}
-    behavior={Platform.OS === 'ios' ? 'padding' : null}
-  >
-    
-
-          <View style={styles.header}>
-        <Ionicons name="arrow-back" size={24} color="#fff" onPress={() => navigation.goBack()} />
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : null}>
+      <View style={styles.header}>
+        <Ionicons
+          name="arrow-back"
+          size={24}
+          color="#955629"
+          onPress={() => navigation.goBack()}
+        />
         <Text style={styles.headerText}>Add Coffee</Text>
       </View>
-          <ScrollView style={styles.scrollView}>
-       <View style={styles.column}>
-  <Picker
-    style={styles.picker}
-    selectedValue={selectedCategory}
-    onValueChange={(itemValue) => setSelectedCategory(itemValue)}
-  >
-    {coffeeCategories.map((category) => (
-      <Picker.Item key={category.id} label={category.name} value={category.id} />
-    ))}
-  </Picker>
-  <TouchableOpacity onPress={handleImageSelection} style={styles.uploadImage}>
-  <View style={styles.imageContainer}>
-    {imageUri ? (
-      <Image source={{ uri: imageUri }} style={styles.image} />
-    ) : (
-      <Ionicons name="person-circle-outline" size={wp(30)} color="#955629" />
-    )}
-    <Text style={styles.uploadText}>
-      {imageUri ? 'Change Image' : 'Upload Your Image Here'}
-    </Text>
-  </View>
-</TouchableOpacity>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.column}>
+          <TouchableOpacity
+            onPress={handleImageSelection}
+            style={styles.uploadImage}>
+            <View style={styles.imageContainer}>
+              {imageUri ? (
+                <Image source={{uri: imageUri}} style={styles.image} />
+              ) : (
+                <Ionicons name="images" size={wp(13)} color="#955629" />
+              )}
+              <Text style={styles.uploadText}>
+                {imageUri ? 'Change Image' : 'Upload the image here'}
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.exampleText}> Select a category</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={selectedCategory}
+            onValueChange={itemValue => setSelectedCategory(itemValue)}>
+            {coffeeCategories.map(category => (
+              <Picker.Item
+                key={category.id}
+                label={category.name}
+                value={category.id}
+              />
+            ))}
+          </Picker>
 
-  {/* Input fields for other coffee properties */}
-  <TextInput
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-   style={styles.input}
-     placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-    placeholder="Name" value={name} onChangeText={setName} />
-  <TextInput 
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-  style={styles.input} placeholder="Description" value={description} onChangeText={setDescription} />
+          {/* Input fields for other coffee properties */}
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            placeholder="Name"
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholder="Description"
+            value={description}
+            onChangeText={setDescription}
+          />
 
-
-  <TextInput
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-   style={styles.input} placeholder="Price" value={price} onChangeText={setPrice} />
-  <TextInput style={styles.input}
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-   placeholder="Ingredients" value={ingredients} onChangeText={setIngredients} />
-  <TextInput
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-   style={styles.input} placeholder="Serving Size" value={servingSize} onChangeText={setServingSize} />
-  <TextInput
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-   style={styles.input} placeholder="Caffeine Content" value={caffeineContent} onChangeText={setCaffeineContent} />
-  <TextInput 
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-  style={styles.input} placeholder="Origin" value={origin} onChangeText={setOrigin} />
-  <TextInput
-    placeholderTextColor={isDarkMode ? '#fff' : '#999'}
-  
-   style={styles.input} placeholder="Roast Level" value={roastLevel} onChangeText={setRoastLevel} />
-  <Button title="Add Coffee"
-
-  color="#955629"
-
-  
-      
-   onPress={handleAddCoffee} />
-</View>
- </ScrollView>
-   </KeyboardAvoidingView>
-
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholder="Price"
+            value={price}
+            onChangeText={setPrice}
+          />
+          <TextInput
+            style={styles.input}
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            placeholder="Ingredients"
+            value={ingredients}
+            onChangeText={setIngredients}
+          />
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholder="Serving Size"
+            value={servingSize}
+            onChangeText={setServingSize}
+          />
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholder="Caffeine Content"
+            value={caffeineContent}
+            onChangeText={setCaffeineContent}
+          />
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholder="Origin"
+            value={origin}
+            onChangeText={setOrigin}
+          />
+          <TextInput
+            placeholderTextColor={isDarkMode ? '#fff' : '#999'}
+            style={styles.input}
+            placeholder="Roast Level"
+            value={roastLevel}
+            onChangeText={setRoastLevel}
+          />
+          <Button
+            title="Add Coffee"
+            color="#955629"
+            onPress={handleAddCoffee}
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
-
-
-
-
-
 
 export default AddCoffeeScreen;
--- src/screen/admin/HomeAdmin.tsx
+++ src/screen/admin/HomeAdmin.tsx
@@ -1,12 +1,93 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, StyleSheet, Image} from 'react-native';
+import SQLite from 'react-native-sqlite-2';
+
+const db = SQLite.openDatabase('users.db');
 
 const HomeAdmin = () => {
-  return (
-    <View>
-      <Text>HomeAdmin</Text>
-    </View>
-  )
-}
+  const [coffeeItems, setCoffeeItems] = useState([]);
 
-export default HomeAdmin
+  useEffect(() => {
+    fetchCoffeeItems();
+  }, []);
+
+  const fetchCoffeeItems = () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM coffeeData',
+        [],
+        (_, {rows}) => {
+          const data = [];
+
+          for (let i = 0; i < rows.length; i++) {
+            data.push(rows.item(i));
+       
+          }
+          setCoffeeItems(data);
+          console.log('====================================');
+          console.log(data);
+          console.log('====================================');
+        },
+        (_, error) => {
+          console.log('Error fetching coffee data:', error);
+        },
+      );
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>HomeAdmin</Text>
+      <FlatList
+        data={coffeeItems}
+        renderItem={({item}) => (
+          <View style={styles.coffeeItem}>
+            <Image source={{uri: item.imageUri}} style={styles.itemImage} />
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemDescription}>{item.description}</Text>
+            <Text style={styles.itemPrice}>${item.price}</Text>
+            <Text style={styles.itemOrigin}>{item.origin}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  coffeeItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  itemName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  itemDescription: {
+    marginTop: 4,
+    color: '#555',
+  },
+  itemPrice: {
+    color: '#955629',
+  },
+  itemOrigin: {
+    marginTop: 4,
+    color: '#555',
+  },
+});
+
+export default HomeAdmin;
